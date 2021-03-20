c9816e01a49b56d43004c85220e2e66a98cf09af
vavrines <tianbaixiao@gmail.com>
2021-03-20 11:05:24 +0100

    Sync

--- code/cavity/det.jl
+++ code/cavity/det.jl
@@ -1,6 +1,6 @@
-using OrdinaryDiffEq, Kinetic
+using OrdinaryDiffEq, CUDA, Kinetic
 using KitBase.ProgressMeter
-using KitML.DiffEqFlux
+using KitML.Flux, KitML.DiffEqFlux
 
 cd(@__DIR__)
 ks, ctr, a1face, a2face, t = initialize("config.txt")
@@ -14,22 +14,20 @@ nt = (ks.set.maxTime ÷ dt) |> Int
     Kinetic.update!(ks, ctr, a1face, a2face, dt, res; coll = Symbol(ks.set.collision), bc = Symbol(ks.set.boundary))
     global t += dt
 end
-
 plot_contour(ks, ctr)
-
-
 
 n0 = ks.vSpace.nu * ks.vSpace.nu
 nh = 8
 
 nn = FastChain(FastDense(n0, n0*nh, tanh), FastDense(n0*nh, n0*nh, tanh), FastDense(n0*nh, n0))
-p = initial_params(nn)
+p = initial_params(nn) |> CuArray
 
 X = zeros(Float32, n0, ks.pSpace.nx*ks.pSpace.ny)
 for i = 1:ks.pSpace.nx, j = 1:ks.pSpace.ny
     idx = ks.pSpace.ny * (i - 1) + j
     X[:, idx] .= ctr[i, j].f[:]
 end
+XC = CuArray(X)
 
 M = Array{Float32}(undef, n0, ks.pSpace.nx*ks.pSpace.ny)
 S = Array{Float32}(undef, n0, ks.pSpace.nx*ks.pSpace.ny)
@@ -43,13 +41,44 @@ Threads.@threads for j = 1:ks.pSpace.ny
         τ[1, idx] = vhs_collision_time(ctr[i, j].prim, ks.gas.μᵣ, ks.gas.ω)
     end
 end
-P = [M, τ]
+MC = CuArray(M)
+SC = CuArray(S)
+τC = CuArray(τ)
 
-prob = ODEProblem(bgk_ode!, X, (0.0, dt), (S, τ))
-Y = solve(prob, Midpoint(), saveat=0:dt/2:dt) |> Array
+prob = ODEProblem(bgk_ode!, XC, (0.0, dt), (SC, τC))
+Y = solve(prob, Euler(), dt=dt).u[end]
 
 function dfdt(df, f, p, t)
-    df .= ((M .- f) ./ τ .+ nn(f, p))
+    df .= ((MC .- f) ./ τC .+ nn(f, p))
 end
 
-reshape(M[:, 1], ks.vSpace.nu, ks.vSpace.nv)
+prob_ube = ODEProblem(dfdt, XC, (0.0, dt), p)
+function loss(p)
+    #sol_ube = solve(prob_ube, Midpoint(), u0=X, p=p, saveat=0:dt/2:dt)
+    sol_ube = solve(prob_ube, Euler(), u0=XC, p=p, dt=dt)
+    
+    return sum(abs2, sol_ube.u[end] .- Y)
+end
+
+cb = function (p, l)
+    println("loss: $l")
+    return false
+end
+
+res = sci_train(loss, p, ADAM(); cb=Flux.throttle(cb, 1), maxiters=200)
+
+
+
+
+
+pc = res.u |> CuArray
+XC = CuArray(X)
+YC = CuArray(Y)
+
+prob_cuda = ODEProblem(dfdt, XC, (0.0, dt), pc)
+function loss(p)
+    sol = solve(prob_cuda, Euler(), u0=XC, p=pc, dt=dt)
+    return sum(abs2, Array(sol) .- YC)
+end
+
+solve(prob_cuda, Euler(), u0=XC, p=pc, dt=dt)
