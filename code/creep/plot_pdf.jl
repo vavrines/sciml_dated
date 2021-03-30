a7d30fa71cf770956948f499f84e6242d61ff0e3
vavrines <tianbaixiao@gmail.com>
2021-03-30 11:06:26 +0200

    Sync

--- code/creep/plot_pdf.jl
+++ code/creep/plot_pdf.jl
@@ -1,7 +1,8 @@
-using Kinetic, PyCall
-using JLD2
+using Kinetic
+using KitBase.PyCall, KitBase.JLD2
 
 begin
+    cd(@__DIR__)
     set = Setup(
         "gas", # matter
         "creep", # case
@@ -30,8 +31,7 @@ begin
     ib = IB2F(w0, prim0, h0, b0, bcL, w0, prim0, h0, b0, bcR)
 
     ks = SolverSet(set, ps, vs, gas, ib, @__DIR__)
-    cd(@__DIR__)
-    @load "transition/ctr.jld2"
+    @load "continuum/ctr.jld2"
 end
 
 u = zeros(ks.vSpace.nu, ks.vSpace.nv, ks.pSpace.nx)
@@ -46,6 +46,69 @@ pdf = zeros(ks.vSpace.nu, ks.vSpace.nv, ks.pSpace.nx)
 for k in axes(pdf, 3)
     pdf[:, :, k] .= (ctr[k, 20].h .+ ctr[k, 21].h) ./ 2
 end
+
+M = zeros(ks.vSpace.nu, ks.vSpace.nv, ks.pSpace.nx)
+for k in axes(M, 3)
+    M[:, :, k] .= maxwellian(ks.vSpace.u, ks.vSpace.v, (ctr[k, 20].prim .+ ctr[k, 21].prim) ./ 2)
+end
+S = zeros(ks.vSpace.nu, ks.vSpace.nv, ks.pSpace.nx)
+for k in axes(S, 3)
+    q = heat_flux(pdf[:, :, k], (ctr[k, 20].prim .+ ctr[k, 21].prim) ./ 2, 
+        ks.vSpace.u, ks.vSpace.v, ks.vSpace.weights)
+    S[:, :, k] .= shakhov(ks.vSpace.u, ks.vSpace.v, M[:, :, k], q, (ctr[k, 20].prim .+ ctr[k, 21].prim) ./ 2, 2/3)
+end
+τ = zeros(ks.pSpace.nx)
+for i in eachindex(τ)
+    τ[i] = vhs_collision_time((ctr[i, 20].prim .+ ctr[i, 21].prim) ./ 2, ks.gas.μᵣ, ks.gas.ω)
+end
+QB = zeros(ks.vSpace.nu, ks.vSpace.nv, ks.pSpace.nx)
+QS = zeros(ks.vSpace.nu, ks.vSpace.nv, ks.pSpace.nx)
+for k in axes(M, 3)
+    QB[:, :, k] .= (M[:, :, k] - pdf[:, :, k]) / τ[k]
+    QS[:, :, k] .= (M[:, :, k] .+ S[:, :, k] - pdf[:, :, k]) / τ[k]
+end
+
+plotly = pyimport("plotly")
+go = plotly.graph_objects
+fig = go.Figure()
+fig.add_trace(
+    go.Volume(
+        x=u[:],
+        y=v[:],
+        z=x[:],
+        value=QB[:],
+        opacity=0.1,
+        surface_count=31, 
+    )
+)
+fig.update_layout(
+    scene = Dict(
+        "xaxis_title"=>"u",
+        "yaxis_title"=>"v",
+        "zaxis_title"=>"x"
+    ),
+)
+fig.show()
+fig.write_image("creep_bgk1.pdf")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 plotly = pyimport("plotly")
 go = plotly.graph_objects
