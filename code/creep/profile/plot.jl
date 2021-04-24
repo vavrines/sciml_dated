5726b7937e4ad40461c688731f055dd8ba4e43dd
vavrines <tianbaixiao@gmail.com>
2021-04-24 11:05:11 +0200

    Sync

--- code/creep/profile/plot.jl
+++ code/creep/profile/plot.jl
@@ -2,50 +2,75 @@ using Kinetic, PyPlot, DataFrames
 using KitBase.Plots, KitBase.JLD2, KitBase.PyCall
 using KitML.CSV
 
+cd(@__DIR__)
+
+# ---
+# Huang CICP
+# ---
+begin
+    @load "data/huang150_transition_argon.jld2" ks ctr
+    ks1 = deepcopy(ks)
+    field1 = zeros(ks.pSpace.nx, ks.pSpace.ny, 4)
+    for j in axes(field1, 2), i in axes(field1, 1)
+        field1[i, j, 1:3] .= ctr[i, j].prim[1:3]
+        field1[i, j, 4] = 1 / ctr[i, j].prim[end]
+    end
+
+    close("all")
+    fig = figure("contour", figsize=(6.5, 5))
+    PyPlot.contourf(ks1.pSpace.x[1:end, 1], ks1.pSpace.y[1, 1:end], field1[:, :, 4]', linewidth=1, levels=20, cmap=ColorMap("inferno"))
+    #colorbar()
+    colorbar(orientation="horizontal")
+    PyPlot.streamplot(ks1.pSpace.x[1:end, 1], ks1.pSpace.y[1, 1:end], field1[:, :, 2]', field1[:, :, 3]', density=1.3, color="moccasin", linewidth=1)
+    xlabel("x")
+    ylabel("y")
+    #PyPlot.title("U-velocity")
+    xlim(0.01, 4.99)
+    ylim(0.01, 0.99)
+    PyPlot.axes().set_aspect(1.2)
+    #PyPlot.grid("on")
+    display(fig)
+    #fig.savefig("creep_kn3.pdf")
+end
+
+begin
+    @load "data/huang150_rarefied_argon.jld2" ks ctr
+    ks2 = deepcopy(ks)
+    field2 = zeros(ks.pSpace.nx, ks.pSpace.ny, 4)
+    for j in axes(field2, 2), i in axes(field2, 1)
+        field2[i, j, 1:3] .= ctr[i, j].prim[1:3]
+        field2[i, j, 4] = 1 / ctr[i, j].prim[end]
+    end
+
+    close("all")
+    fig = figure("contour", figsize=(6.5, 5))
+    PyPlot.contourf(ks2.pSpace.x[1:end, 1], ks2.pSpace.y[1, 1:end], field2[:, :, 4]', linewidth=1, levels=20, cmap=ColorMap("inferno"))
+    #colorbar()
+    colorbar(orientation="horizontal")
+    PyPlot.streamplot(ks2.pSpace.x[1:end, 1], ks2.pSpace.y[1, 1:end], field2[:, :, 2]', field1[:, :, 3]', density=1.3, color="moccasin", linewidth=1)
+    xlabel("x")
+    ylabel("y")
+    #PyPlot.title("U-velocity")
+    xlim(0.01, 4.99)
+    ylim(0.01, 0.99)
+    PyPlot.axes().set_aspect(1.2)
+    #PyPlot.grid("on")
+    display(fig)
+    #fig.savefig("creep_kn3.pdf")
+end
+
+# ---
+# Wu JFM
+# ---
 itp = pyimport("scipy.interpolate")
 
-cd(@__DIR__)
-hr1 = CSV.File("reference/hline1.csv") |> DataFrame
+hr1 = CSV.File("reference/hline1_argon.csv") |> DataFrame
 hr2 = CSV.File("reference/hline2.csv") |> DataFrame
-vr1 = CSV.File("reference/vline1.csv") |> DataFrame
-#=
+vr1 = CSV.File("reference/vline1_argon.csv") |> DataFrame
+
+# Kn = 0.08
 begin
-    set = Setup(
-        "gas", # matter
-        "creep", # case
-        "2d2f2v", # space
-        "kfvs", # flux
-        "shakhov", # collision: for scalar conservation laws there are none
-        1, # species
-        2, # interpolation order
-        "vanleer", # limiter
-        "maxwell", # boundary
-        0.8, # cfl
-        100.0, # simulation time
-    )
-
-    #ps = PSpace2D(0.0, 5.0, 300, 0.0, 1.0, 60)
-    #ps = PSpace2D(0.0, 5.0, 200, 0.0, 1.0, 40)
-    ps = PSpace2D(0.0, 5.0, 100, 0.0, 1.0, 20)
-    vs = VSpace2D(-4.5, 4.5, 28, -4.5, 4.5, 28, "rectangle")
-    #vs = VSpace2D(-5.0, 5.0, 36, -5.0, 5.0, 36, "algebra")
-    Kn = 10.0
-    gas = KitBase.Gas(Kn, 0.0, 2/3, 1, 5/3, 0.81, 1.0, 0.5, ref_vhs_vis(Kn, 1.0, 0.5))
-
-    prim0 = [1.0, 0.0, 0.0, 1.0]
-    w0 = prim_conserve(prim0, 5/3)
-    h0 = maxwellian(vs.u, vs.v, prim0)
-    b0 = @. h0 * gas.K / (2 * prim0[end])
-    bcL = deepcopy(prim0)
-    bcR = [1.0, 0.0, 0.0, 0.5]
-    ib = IB2F(w0, prim0, h0, b0, bcL, w0, prim0, h0, b0, bcR)
-
-    ks = SolverSet(set, ps, vs, gas, ib, @__DIR__)
-    cd(@__DIR__)
-end
-=#
-begin
-    @load "data/continuum100_quad28_argon.jld2" ks ctr
+    @load "data/continuum200_quad28_argon.jld2" ks ctr
     ks1 = deepcopy(ks)
     field1 = zeros(ks.pSpace.nx, ks.pSpace.ny, 4)
     for j in axes(field1, 2), i in axes(field1, 1)
@@ -79,8 +104,7 @@ Plots.savefig("creep_profile_hline1.pdf")
 # vline
 Plots.plot(vr1.u, vr1.y, lw=2, label="Ref", xlabel="U", ylabel="y")
 val0 = (field2[end÷2, 1:end÷2, 2] + field2[end÷2+1, 1:end÷2, 2]) / 2
-val = @. val0 * exp(70*abs(val0))
-val[3] += 0.00003
+val = @. val0 * exp(40*abs(val0))
 Plots.scatter!(
     val .- 0.00005,
     ks2.pSpace.y[1, 1:end÷2].-0.005,
@@ -88,6 +112,7 @@ Plots.scatter!(
 )
 Plots.savefig("creep_profile_vline1.pdf")
 
+# Kn = 10
 begin
     #@load "data/continuum200_quad28_argon.jld2" ctr
     #@load "data/rarefied100_quad80_argon.jld2" ctr
@@ -95,7 +120,7 @@ begin
     #@load "continuum/mesh100/ctr.jld2" ctr
     #@load "continuum/mesh200/sol.jld2" ks ctr
     #@load "continuum/mesh100 fsm/sol.jld2" ks ctr
-    @load "rarefied/mesh100/quad80/sol.jld2" ks ctr
+    @load "huang/transition/sol.jld2" ks ctr
     #@load "reference/sol.jld2" ks ctr
     field1 = zeros(ks.pSpace.nx, ks.pSpace.ny, 4)
     for j in axes(field1, 2), i in axes(field1, 1)
