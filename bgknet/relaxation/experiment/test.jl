ef6109f80a962b28d21956f2fb56919c516a5a62
vavrines <tianbaixiao@gmail.com>
2022-08-30 18:18:47 +0200

    Sync

--- bgknet/relaxation/experiment/test.jl
+++ bgknet/relaxation/experiment/test.jl
@@ -1,6 +1,8 @@
-using Kinetic, Solaris, OrdinaryDiffEq
+using Kinetic, Solaris, OrdinaryDiffEq, Plots
 using Solaris.Flux: Adam, throttle
 using Solaris.Optimization, ReverseDiff
+
+cd(@__DIR__)
 
 set = (
     u0 = -5,
@@ -72,69 +74,26 @@ data_bgk = solve(prob1, Tsit5(), saveat = tsteps) |> Array
 prob2 = ODEProblem(bgk_ode!, [h0; b0], tspan, ([H0 + SH; B0 + SB], τ0))
 data_shakhov = solve(prob2, Tsit5(), saveat = tsteps) |> Array
 
-nm = vs.nu * 2
-nν = vs.nu * 2 + 1
-
-mn = FnChain(FnDense(nm, nm, tanh; bias = false), FnDense(nm, nm, tanh; bias = false), FnDense(nm, nm; bias = false))
-νn = FnChain(FnDense(nν, nν, tanh; bias = false), FnDense(nν, nν, tanh; bias = false), FnDense(nν, nm; bias = false))
-nn = BGKNet(mn, νn)
-#nn = BGKReduceNet(mn, νn)
-u = init_params(nn)
-
-X1 = Array{Float64}(undef, vs.nu*2, size(data_boltz)[end])
-for i in axes(X1, 2)
-    X1[1:vs.nu, i], X1[vs.nu+1:end, i] = reduce_distribution(data_boltz[:, :, :, i], vs3.v, vs3.w, vs2.weights)
-end
-τ = Array{Float64}(undef, 1, size(X1, 2))
-for i in axes(τ, 2)
-    τ[1, i] = τ0
-end
-X1 = vcat(X1, ones(Float64, 1, size(X1, 2)) .* τ[1])
-
-rhs3D = zeros(Float32, vs3.nu, vs3.nv, vs3.nw, size(X1, 2))
-for i in axes(rhs3D, 4)
-    df = @view rhs3D[:, :, :, i]
-    boltzmann_ode!(df, data_boltz[:, :, :, i], [kn_bzm, set.nm, phi, psi, chi], 0.0)
-end
-
-Y1 = Array{Float64}(undef, vs.nu*2, size(data_shakhov, 2))
-for j in axes(Y1, 2)
-    Y1[1:vs.nu, j], Y1[vs.nu+1:end, j] = reduce_distribution(rhs3D[:, :, :, j], vs3.v, vs3.w, vs2.weights)
-end
-
-L = size(Y1, 2)
-
-function loss(p, x, y)
-    pred = nn(x, p, vs, γ)
-    return sum(abs2, pred - y) / L
-end
-loss(p) = loss(p, X1, Y1)
-
-loss(u)
-
-cb = function (p, l)
-    println("loss: $(l)")
-    return false
-end
-cb = throttle(cb, 1)
-
-res = sci_train(loss, u, Adam(); cb = cb, ad = Optimization.AutoReverseDiff(), iters = 500)
-res = sci_train(loss, res.u, Adam(); cb = cb, ad = Optimization.AutoReverseDiff(), iters = 2000)
+@load "prototype.jld2" nn u
 
 function dfdt(df, f, p, t)
     nn, u, vs, γ = p
     df .= nn([f; τ0], u, vs, γ)
 end
 
-ube = ODEProblem(dfdt, [h0; b0], tspan, (nn, res.u, vs, 5/3))
+ube = ODEProblem(dfdt, [h0; b0], tspan, (nn, u, vs, 5/3))
 sol = solve(ube, Midpoint(); saveat = tsteps)
 
-using Plots
-
-idx = 12
+idx = 2
 begin
     plot(vs.u, sol[idx][1:vs.nu], color = :gray32, label = "NN")
     plot!(vs.u, data_bgk[1:vs.nu, idx], color = 1, label = "BGK")
     plot!(vs.u, data_shakhov[1:vs.nu, idx], color = 2, line = :dash, label = "Shakhov")
     scatter!(vs.u, data_boltz_1D[1:vs.nu, idx], color = 3, label = "Boltzmann", alpha = 0.6)
+end
+
+begin
+    pred = nn([h0; b0; τ0], u, vs, 5/3)
+#    pred = nn([h0; b0; τ0], zero(u), vs, 5/3)
+    plot(pred[1:vs.nu])
 end
