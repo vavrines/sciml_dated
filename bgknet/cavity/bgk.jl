c92e97c49dfb8bd3d32fabca62407d82c03b69e1
vavrines <tianbaixiao@gmail.com>
2022-10-26 09:28:37 +0200

    Sync

---
+++ bgknet/cavity/bgk.jl
@@ -0,0 +1,22 @@
+using KitBase
+using KitBase.JLD2
+using KitBase.ProgressMeter: @showprogress
+
+cd(@__DIR__)
+ks, ctr, a1face, a2face, t = initialize("config_bgk.txt")
+
+res = zeros(4)
+dt = timestep(ks, ctr, t)
+nt = floor(ks.set.maxTime / dt) |> Int
+@showprogress for iter = 1:500#nt
+    reconstruct!(ks, ctr)
+    evolve!(ks, ctr, a1face, a2face, dt)
+    update!(ks, ctr, a1face, a2face, dt, res)
+
+    maximum(res) < 5e-7 && break
+end
+
+using Plots
+plot(ks, ctr)
+
+@save "bgk.jld2" ks ctr
